d8cf612b7c04f356f52e7ae692b64387726ae214
cipiisekk <papousek.mail@gmail.com>
2026-02-06 22:46:20 +0100

    added config

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+ // use quotation marks for define
+#define NAMEFILE "todo"
+#define FILEEXTENSION ".txt" // (use with dot) if u dont want one -> just type nothing or use ""
+
+#endif
