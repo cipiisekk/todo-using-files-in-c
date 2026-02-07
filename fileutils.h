8641620ae0367e6370c5a4d5fa72d458325f81af
cipiisekk <papousek.mail@gmail.com>
2026-02-07 02:16:26 +0100

    added headers to headers

--- fileutils.h
+++ fileutils.h
@@ -1,6 +1,10 @@
 #ifndef FILEUTILS_H
 #define FILEUTILS_H
 
+#include "config.h"
+#include <stdio.h>
+#include <stdbool.h>
+
 bool checkIfOk(FILE* f, const char* what);
 bool createFiles(void);
 FILE* openTodo(const char* mode);
--- todo.h
+++ todo.h
@@ -1,6 +1,10 @@
 #ifndef TODO_H
 #define TODO_H
 
+#include "fileutils.h"
+#include <stdio.h>
+#include <stdbool.h>
+
 void add(const char *item);
 void list(void);
 void remove(int id);
